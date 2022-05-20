70387781abcf47a717f8f84a6ee8cdec2a365e0b
chenshengen <enengege@gmail.com>
2022-05-20 16:06:17 +0800

    pool does not need to reconnect

--- pool.go
+++ pool.go
@@ -1,101 +1,80 @@
-package grpc
+package grpcpool
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"sync/atomic"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/connectivity"
 )
 
-// 如何预估连接池大小：
-//   每个 gRPC channel 可能使用多个 HTTP/2 连接，连接的数量基于该服务器解析的IP数量，
-//   每个连接通常限制 100 个并发的 stream (可以用 MaxConcurrentStreams 修改)
-//   假设目标服务器只有 1 个IP，gRPC channel 使用 1 条连接，平均每条连接处理 50 个并发请求，
-//   需要的 channel 数量是 ceil(并发请求数 / 50)
-//   例如预估有 100 个并发请求，需要 ceil(100 / 50) == 2 个 channel，连接池大小为 2
-const defaultSize = 2
+// reconnection could be done by adding dial option: grpc.WithConnectParams
+// no need to do it in the pool.
 
-// gRPC 连接池，实现多个 channel 循环发送请求
-type channelPool struct {
-	size      int
-	i         uint32
-	channels  []*grpc.ClientConn
-	factory   ChannelFactoryFunc
-	reconnect chan int // inside is the index of the gRPC channel which need to reconnect
-	done      chan struct{}
+// 如何预估连接池大小：
+//   每个 gRPC connection 可能使用多个 HTTP/2 连接，连接的数量基于该服务器解析的IP数量，
+//   每个连接通常限制 100 个并发的 stream (可以用 MaxConcurrentStreams 修改)
+//   假设目标服务器只有 1 个IP，gRPC connection 使用 1 条连接，平均每条连接处理 50 个并发请求，
+//   需要的 connection 数量是 ceil(并发请求数 / 50)
+//   例如预估有 100 个并发请求，需要 ceil(100 / 50) == 2 个 connection，连接池大小为 2
+const defaultPoolSize = 2
+
+// gRPC 连接池
+type connPool struct {
+	size    int
+	i       uint32
+	conns   []*grpc.ClientConn
+	factory connFactoryFunc
+	done    chan struct{}
 }
 
-type ChannelFactoryFunc func() (*grpc.ClientConn, error)
+type connFactoryFunc func() (*grpc.ClientConn, error)
 
-func New(size int, factory ChannelFactoryFunc) *channelPool {
+func newConnPool(size int, factory connFactoryFunc) *connPool {
 	if size <= 0 {
-		size = defaultSize
+		size = defaultPoolSize
 	}
 
-	p := &channelPool{
-		size:      size,
-		channels:  make([]*grpc.ClientConn, size),
-		factory:   factory,
-		reconnect: make(chan int, size),
-		done:      make(chan struct{}),
+	p := &connPool{
+		size:    size,
+		conns:   make([]*grpc.ClientConn, size),
+		factory: factory,
+		done:    make(chan struct{}),
 	}
-	go p.reconnectLoop()
 
 	for i := 0; i < size; i++ {
 		c, err := factory()
 		if err != nil {
-			fmt.Printf("error creating channel: %s\n", err)
+			log.Printf("connect grpc: %s", err)
 			continue
 		}
-		p.channels[i] = c
+		p.conns[i] = c
 	}
 	return p
 }
 
-func isAvailable(c *grpc.ClientConn) bool {
+func isValid(c *grpc.ClientConn) bool {
 	return c != nil && c.GetState() != connectivity.Shutdown
 }
 
-func (p *channelPool) reconnectLoop() {
-	for {
-		select {
-		case <-p.done:
-			return
-		case i := <-p.reconnect:
-			if isAvailable(p.channels[i]) {
-				// another Get has found it unavailable and command to reconnect
-				continue
-			}
-			channel, err := p.factory()
-			if err != nil {
-				fmt.Printf("error reconnecting: %s\n", err)
-				continue
-			}
-			p.channels[i] = channel
-		}
-	}
-}
-
-func (p *channelPool) Get() *grpc.ClientConn {
+func (p *connPool) Get() (*grpc.ClientConn, error) {
 	i := int(atomic.AddUint32(&p.i, 1)) % p.size
-	channel := p.channels[i]
-	if !isAvailable(channel) {
-		p.reconnect <- i
-		// try to get another one
-		i = int(atomic.AddUint32(&p.i, 1)) % p.size
-		channel = p.channels[i]
+	conn := p.conns[i]
+	if !isValid(conn) {
+		return nil, errors.New("invalid grpc connection")
 	}
-	return channel
+	return conn, nil
 }
 
-func (p *channelPool) Close() error {
+func (p *connPool) Close() error {
 	close(p.done)
 	var err error
-	for _, c := range p.channels {
+	for _, c := range p.conns {
 		if e := c.Close(); e != nil {
-			// do not return until all channel closed
+			// still need to close other connections, do not return here
 			err = e
+			log.Printf("close grpc connection: %s", e)
 		}
 	}
 	return err
